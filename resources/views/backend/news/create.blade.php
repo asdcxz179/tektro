ba155d4e5e2356b200b876fa5754c44f0adfa31e
cannon <p12576999@gmail.com>
2023-01-12 16:53:16 +0800

    a

--- resources/views/backend/news/create.blade.php
+++ resources/views/backend/news/create.blade.php
@@ -44,7 +44,7 @@
                         </div>                         
                         <div class="form-group col-md-6">
                             <label>{{ __("backend.$routeNameData.banner") }}</label>    
-                            <!-- <div class="text-danger">{{ __('suggested_size', ['width' => 60, 'height' => 60]) }}</div> -->
+                            <div class="text-danger">{{ __('suggested_size', ['width' => 700, 'height' => 500]) }}</div>
                             <fieldset class="image">
                                 <input type="file" name="banner" accept="image/*" />    
                             </fieldset>  
