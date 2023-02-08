b3f74850f3aac4c657824cc5ec48cd73c032d786
Henry <asdcxz179@gmail.com>
2023-02-08 18:11:31 +0800

    a

---
+++ database/migrations/2023_02_08_180859_update_product_tags_20230208_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product_tags', function (Blueprint $table) {
+            $table->tinyInteger('type')->default(1)->after('id')->comment('1:自行車樂別 | 2:特殊類別');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product_tags', function (Blueprint $table) {
+            $table->dropColumn('type');
+        });
+    }
+};
